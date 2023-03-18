e830d58384bc93c7a14526a1476bba2450d7be50
OmarVenturaP <99059639+OmarVenturaP@users.noreply.github.com>
2023-03-17 23:15:44 -0600

    Creando modelo para los productos en la base de datos

---
+++ models/productModels.js
@@ -0,0 +1,13 @@
+// Importandoo Data Types de Sequelize
+import { DataTypes } from "sequelize";
+// importando la Base de datos
+import db from "../database/db";
+
+const productModel = db.define('productos', {
+    nombre: { type: DataTypes.STRING, },
+    precio: { type: DataTypes.INTEGER, },
+    descripcion: { type: DataTypes.STRING, },
+    cantidad: { type: DataTypes.INTEGER, },
+})
+
+export default productModel;

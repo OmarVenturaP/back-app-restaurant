1717020009dc5cca85f20d936fbe90f7a822d196
OmarVenturaP <99059639+OmarVenturaP@users.noreply.github.com>
2023-03-17 22:03:18 -0600

    Agregando modelos de conexion para usuarios

---
+++ models/userModels.js
@@ -0,0 +1,14 @@
+// importamos la conexion a la DB
+import db from "../database/db";
+// importamos Sequelize
+import { DataTypes } from "sequelize";
+
+const userModel = db.define('usuarios', {
+    username: { type: DataTypes.STRING, },
+    nombre: { type: DataTypes.STRING, },
+    apellido: { type: DataTypes.STRING, },
+    email: { type: DataTypes.STRING, },
+    contraseña: { type: DataTypes.STRING, }
+})
+
+export default userModel;

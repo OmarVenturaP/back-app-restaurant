789131106bc0699c248a587f1f431bf90e0ae7f1
OmarVenturaP <99059639+OmarVenturaP@users.noreply.github.com>
2023-03-30 22:53:19 -0600

    deploy at heroku

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = new Sequelize ( process.env.DB_NAME , process.env.DB_USER , process.env.DB_PASS, {
-    host: process.env.DB_HOST,
+    host: "us-cdbr-east-06.cleardb.net",
     dialect: 'mysql'
 });
 
